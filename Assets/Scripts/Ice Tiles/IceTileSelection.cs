696de2211a22c417c82fc584e0866d2696ffc1e7
Caio Guedes <caioguedesam@gmail.com>
2021-06-16 19:12:38 -0300

    Fixing another tile selection visual bug

--- Assets/Scripts/Ice Tiles/IceTileSelection.cs
+++ Assets/Scripts/Ice Tiles/IceTileSelection.cs
@@ -29,28 +29,31 @@ namespace Biweekly
 		private void OnMouseEnter()
 		{
 			if (!IsSelectable) return;
-			_onHover = true;
-			_onMouseEnteredTile.Invoke(gameObject);
+			EnterHover();
 		}
 
 		private void OnMouseOver()
 		{
-			if (!IsSelectable || _onHover) return;
-			_onHover = true;
-			_onMouseEnteredTile.Invoke(gameObject);
+			if (!IsSelectable && _onHover)
+			{
+				ExitHover();
+			}
+			else if (IsSelectable && !_onHover)
+			{
+				EnterHover();
+			}
 		}
 
 		private void OnMouseExit()
 		{
 			if (!_onHover) return;
-			_onHover = false;
-			_onMouseExitedTile.Invoke(gameObject);
+			ExitHover();
 		}
 
 		private void OnMouseDown()
 		{
-			if (!IsSelectable) return;
-			_onMouseDownOnTile.Invoke(gameObject);
+			if (!_onHover) return;
+			MouseClick();
 		}
 
 		private void OnTriggerEnter(Collider other)
@@ -65,6 +68,23 @@ namespace Biweekly
 			if (!other.CompareTag("Player Neighbor Detection")) return;
 
 			_onPlayerRange = false;
+		}
+
+		private void EnterHover()
+		{
+			_onHover = true;
+			_onMouseEnteredTile.Invoke(gameObject);
+		}
+
+		private void ExitHover()
+		{
+			_onHover = false;
+			_onMouseExitedTile.Invoke(gameObject);
+		}
+
+		private void MouseClick()
+		{
+			_onMouseDownOnTile.Invoke(gameObject);
 		}
 	}
 }
